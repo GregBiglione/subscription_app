8910318050f4ada30fc7bc2cff465cf926c3eb8c
GregBiglione <gregory.biglione81@gmail.com>
2023-10-19 12:11:01 +0200

    Added SubscriptionStatus object

---
+++ lib/domain/model/subscription_status.dart
@@ -0,0 +1,8 @@
+class SubscriptionStatus {
+  bool isActiveSubscription;
+  String status;
+  String activePriceId;
+
+  SubscriptionStatus({required this.isActiveSubscription, required this.status,
+    required this.activePriceId});
+}
